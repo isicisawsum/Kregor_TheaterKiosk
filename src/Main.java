c82eb61d6e8129ea6c24d8f172bebb031c252e38
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-09 18:27:41 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
+import java.util.Scanner; //importing Scanner
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello, welcome to the theater, please enter your age:");
-        int age = in.nextInt();
-        if (age >= 21)
+        System.out.println("Hello, welcome to the theater, please enter your age:"); //Asking user for their age
+        int age = in.nextInt(); //defining variable age
+        if (age >= 21) //if/then statement to detect what age user inputted
         {
-            System.out.println("You get a wristband!");
+            System.out.println("You get a wristband!"); //telling user they got a wristband
         }
-        else if (age > 0 && age < 21)
+        else if (age > 0 && age < 21) //if user inputted number below 21
         {
-            System.out.println("I’m sorry, your age is under 21!");
+            System.out.println("I’m sorry, your age is under 21!"); //telling user the age they inputted is under 21
         }
         else
         {
-            System.out.println("That’s not an age!");
+            System.out.println("That’s not an age!"); //tells user that the age they inputted is not a real age
         }
     }
 }
