e74d35625d103f37a9c323593663f02e7c1420c8
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-09 18:24:18 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello, welcome to the theater, please enter your age:");
+        int age = in.nextInt();
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
+        else if (age > 0 && age < 21)
+        {
+            System.out.println("I’m sorry, your age is under 21!");
+        }
+        else
+        {
+            System.out.println("That’s not an age!");
         }
     }
 }
